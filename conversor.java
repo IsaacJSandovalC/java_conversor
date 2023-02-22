dd6ae152db5403a75141bd99847244e79d51924a
Isaac Sandoval <108201006+IsaacJSandovalC@users.noreply.github.com>
2023-02-22 13:20:09 -0500

    mejora

--- conversor.java
+++ conversor.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
 
 public class conversor extends input {
-    public static void realizarConversion(String monedaObjetivo, double tasaDeCambio) {
+    public static void realizarConversion(String moneda_objetivo, double tasa_de_cambio) {
         try {
             String valor1 = solicitarEntrada("Introduce el valor en dolares");
             double dolares = Double.parseDouble(valor1);
-            double conversion = dolares * tasaDeCambio;
-            JOptionPane.showMessageDialog(null, "El valor en " + monedaObjetivo + " es de: " + conversion);
+            double conversion = dolares * tasa_de_cambio;
+            JOptionPane.showMessageDialog(null, "El valor en " + moneda_objetivo + " es de: " + conversion);
 
             int decision = JOptionPane.showConfirmDialog(null, "¿Estás seguro de que deseas continuar?", "Confirmación",
                     JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
